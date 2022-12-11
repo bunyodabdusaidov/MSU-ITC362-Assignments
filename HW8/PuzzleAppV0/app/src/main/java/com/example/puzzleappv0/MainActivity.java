9398712d3a2a9596791dedff8fe21c1002067acf
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2022-12-11 15:52:27 -0600

    Completed PuzzleAppV0

--- HW8/PuzzleAppV0/app/src/main/java/com/example/puzzleappv0/MainActivity.java
+++ HW8/PuzzleAppV0/app/src/main/java/com/example/puzzleappv0/MainActivity.java
@@ -2,37 +2,50 @@ package com.example.puzzleappv0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
-    public static final String MA = "MainActivity";
+public class MainActivity extends AppCompatActivity {
+    public static int STATUS_BAR_HEIGHT = 24;  // in dp
+    public static int ACTION_BAR_HEIGHT = 56;  // in dp
+    private PuzzleView puzzleView;
+    private Puzzle puzzle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // set up touch event handling
-        View view = findViewById(android.R.id.content);
-        Log.w(MA, "view = " + view);
-        view.setOnTouchListener(this);
-    }
+        puzzle = new Puzzle();
 
-    public boolean onTouch(View v, MotionEvent event) {
-        int action = event.getAction();
-        switch (action) {
-            case MotionEvent.ACTION_DOWN:
-                Log.w(MA, "DOWN: v = " + v + "; event = " + event);
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Log.w(MA, "MOVE: v = " + v + "; event = " + event);
-                break;
-            case MotionEvent.ACTION_UP:
-                Log.w(MA, "UP: v = " + v + "; event = " + event);
-                break;
-        }
-        return true;
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        int screenHeight = size.y;
+        int puzzleWidth = size.x;
+
+        Resources res = getResources();
+        DisplayMetrics metrics = res.getDisplayMetrics();
+        float pixelDensity = metrics.density;
+
+        int actionBarHeight = (int) (pixelDensity * ACTION_BAR_HEIGHT);
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true))
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data, metrics);
+
+        int statusBarHeight = (int) (pixelDensity * STATUS_BAR_HEIGHT);
+        int resourceId = res.getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId != 0)  // found resource for status bar height
+            statusBarHeight = res.getDimensionPixelSize(resourceId);
+
+        int puzzleHeight = screenHeight - statusBarHeight - actionBarHeight;
+        puzzleView = new PuzzleView(this, puzzleWidth, puzzleHeight, puzzle.getNumberParts());
+        String [] scrambled = puzzle.scramble();
+        puzzleView.fillGui(scrambled);
+
+        setContentView(puzzleView);
     }
 }
