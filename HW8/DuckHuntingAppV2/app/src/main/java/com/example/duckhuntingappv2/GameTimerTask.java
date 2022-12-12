662a160a1017daaf4d845bb0eb76cec16c8ec8e0
bunyodabdusaidov <bunyodabdusaidov0@gmail.com>
2022-12-11 20:02:58 -0600

    Completed DuckHuntingAppV2

---
+++ HW8/DuckHuntingAppV2/app/src/main/java/com/example/duckhuntingappv2/GameTimerTask.java
@@ -0,0 +1,26 @@
+package com.example.duckhuntingappv2;
+
+import java.util.TimerTask;
+
+public class GameTimerTask extends TimerTask {
+    private Game game;
+    private GameView gameView;
+
+    public GameTimerTask(GameView view) {
+        gameView = view;
+        game = view.getGame();
+        game.startDuckFromRightTopHalf();
+    }
+
+    @Override
+    public void run() {
+        game.moveDuck();
+        if (game.bulletOffScreen())
+            game.loadBullet();
+        else if (game.isBulletFired())
+            game.moveBullet();
+        if (game.duckOffScreen())
+            game.startDuckFromRightTopHalf();
+        gameView.postInvalidate();
+    }
+}
